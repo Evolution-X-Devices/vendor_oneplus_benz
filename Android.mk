7600a2a96177b870e08e589540d11ffc803f3b0f
CrackedPotato <arnavdrsk7633@gmail.com>
2026-04-02 21:46:29 +0000

    benz: Import from OOS 16.0.2.500

---
+++ Android.mk
@@ -0,0 +1,34 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),benz)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,6e04edc5f2aecdda0146b5e035985b85dd4e68e4)
+$(call add-radio-file-sha1-checked,radio/aop.img,f78319adfe025ba7d28a4155d48ee464f1d5812f)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,ca7360a00b260abedb6aaaf0603fa7d37328071f)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,259a08276ca75740a58381f65e9762c73ff54168)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,6f1f87ff54a5f929c3e0bb19c4c038c6c137bcfe)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,344708d9686e8cbfe1843453baf99bc58eaf60ba)
+$(call add-radio-file-sha1-checked,radio/dsp.img,1b594ddc7882de4846708c13ed10ebea84faf93e)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,5d5c6340950fcb583fddb3a9e1645a1dac9e7ecd)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,08adededcce1e93cfe149745f0a110ca06b28013)
+$(call add-radio-file-sha1-checked,radio/hyp.img,cf814cf029f9e09c640b4d8d0eba7a386cbe9365)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,1c671acbf97409a6ce9ff1c1ad9ddcc8d8ec5449)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,49743764482fed0abc86a675ea9a93bd7664c61c)
+$(call add-radio-file-sha1-checked,radio/modem.img,543707e15360770eabde500d02be09cc6025dbe1)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,98dd4dd4909be893360d24abfef25bf9a3bda318)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,fa394ed65fac958a9f55e6ad0b6a43e02dc3b1ef)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,344e42a4fea7bae5aeab1f752c567b2c6c43add8)
+$(call add-radio-file-sha1-checked,radio/shrm.img,80f230e2ef9be8db6b8ef4dd116e12bd272d1306)
+$(call add-radio-file-sha1-checked,radio/splash.img,84e14653a04d30f754baa9e65b54069f368382bd)
+$(call add-radio-file-sha1-checked,radio/tz.img,ae2b0754de53f1f81dd578f1581a6c91c6dd10ef)
+$(call add-radio-file-sha1-checked,radio/uefi.img,71a69f731a428ad2d73c66d852227215190776d6)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,a1f9ea965520c00f7694bf383a0e2aa027018463)
+$(call add-radio-file-sha1-checked,radio/xbl.img,f6755b368835c2c5917bfe161f73b20a5fd110c3)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,4f7f9ed6709b5528e58dede902f8d20b7df3f113)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,cbde1585e4ebcee05633dc404cd763cf15c086e1)
+
+endif
